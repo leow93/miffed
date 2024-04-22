fcd72ef4cd39f9e75dd29d3fe08307779fd0bd83
leow93 <leowheelan@hotmail.co.uk>
2024-04-22 09:22:10 +0100

    chore: remove some frontend cruft

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useMemo } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type LiftProps = {
+    floors: number
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Lift(props: LiftProps) {
+    const arr = useMemo(() => Array.from({ length: props.floors }, (_, i) => props.floors - i), [props.floors])
+    return (
+        <div className="flex col">
+            {arr.map(floor => (
+                <button key={floor} className="my-2">{floor}</button>
+            ))}
+        </div>
+    )
+
+}
+
+type Lift = {
+    id: number
+    floor: number
+}
+
+function App() {
+    return (
+        <main>
+            <Lift floors={10}/>
+        </main>
+    )
 }
 
 export default App
