df37d3368c4d9975690e4656c0111c91faf84194
leow93 <leowheelan@hotmail.co.uk>
2024-05-04 15:33:22 +0100

    fix: import

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,67 +1,67 @@
-import { useMemo } from "react";
+import {useMemo} from "react";
 import "./App.css";
-import { socket } from "./modules/socket";
-import { useLiftState, useSendMessage } from "./modules/lifts/wiring.ts";
+import {socket} from "./modules/socket";
+import {useLiftState, useSendMessage} from "./modules/lifts/wiring";
 
 type LiftProps = {
-  lowestFloor: number;
-  highestFloor: number;
-  currentFloor: number;
-  onCall: (floor: number) => void;
+    lowestFloor: number;
+    highestFloor: number;
+    currentFloor: number;
+    onCall: (floor: number) => void;
 };
 
 const classNames = (currentFloor: number, floor: number) =>
-  currentFloor === floor ? "bg-blue my-2" : "my-2";
+    currentFloor === floor ? "bg-blue my-2" : "my-2";
 
 function Lift(props: LiftProps) {
-  const arr = useMemo(
-    () =>
-      Array.from(
-        { length: props.highestFloor - props.lowestFloor + 1 },
-        (_, i) => props.highestFloor - i,
-      ),
-    [props.lowestFloor, props.highestFloor],
-  );
-  return (
-    <div className="flex col">
-      {arr.map(floor => (
-        <button
-          key={floor}
-          onClick={() => props.onCall(floor)}
-          className={classNames(props.currentFloor, floor)}
-        >
-          {floor}
-        </button>
-      ))}
-    </div>
-  );
+    const arr = useMemo(
+        () =>
+            Array.from(
+                {length: props.highestFloor - props.lowestFloor + 1},
+                (_, i) => props.highestFloor - i,
+            ),
+        [props.lowestFloor, props.highestFloor],
+    );
+    return (
+        <div className="flex col">
+            {arr.map(floor => (
+                <button
+                    key={floor}
+                    onClick={() => props.onCall(floor)}
+                    className={classNames(props.currentFloor, floor)}
+                >
+                    {floor}
+                </button>
+            ))}
+        </div>
+    );
 }
 
 function App() {
-  const ws = useMemo(() => socket("ws://localhost:8080/socket"), []);
+    const ws = useMemo(() => socket("ws://localhost:8080/socket"), []);
 
-  const state = useLiftState(ws);
-  const sendMessage = useSendMessage(ws);
+    const state = useLiftState(ws);
+    const sendMessage = useSendMessage(ws);
 
-  const onCall = (floor: number) => {
-    sendMessage({
-      type: "call_lift",
-      floor,
-    });
-  };
+    const onCall = (floor: number) => {
+        sendMessage({
+            type: "call_lift",
+            floor,
+        });
+    };
 
-  return (
-    <main>
-      {state.type === "created" && (
-        <Lift
-          lowestFloor={state.lowestFloor}
-          highestFloor={state.highestFloor}
-          onCall={onCall}
-          currentFloor={state.currentFloor}
-        />
-      )}
-    </main>
-  );
+    return (
+        <main>
+            {state.type === "created" && (
+                <Lift
+                    lowestFloor={state.lowestFloor}
+                    highestFloor={state.highestFloor}
+                    onCall={onCall}
+                    currentFloor={state.currentFloor}
+                />
+            )}
+        </main>
+    );
 }
 
 export default App;
